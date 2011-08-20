676348912b44f7fb91a42effb719189b33b606ce
Ed <edvelez.g@gmail.com>
2011-08-20 11:15:08 -0500

    adding number_helper and string support

---
+++ lib/support/number_helper.rb
@@ -0,0 +1,33 @@
+# This module illustrates how additional functionality can be 
+# included (or "mixed-in") to a class and then reused.
+# Borrows heavily from Ruby on Rails' number_to_currency method.
+
+module NumberHelper
+  def number_to_currency(number, options={})
+    unit      = options[:unit] || '$'
+    precision = options[:precision] || 2
+    delimiter = options[:delimiter] || ','
+    separator = options[:separator] || '.'
+    
+    separator = '' if precision == 0
+    integer, decimal = number.to_s.split('.')
+    
+    i = integer.length
+    until i <= 3
+      i -= 3
+      integer = integer.insert(i, delimiter)
+    end
+    
+    if precision == 0
+      precise_decimal = ''
+    end
+    # make sure decimal is not nil
+    decimal ||= "0"
+    
+    # make sure the decimal is not too large
+    decimal = decimal[0, precision-1]
+    
+    # make sure it is not too short
+    precise_decimal = decimal.ljust(precision, "0")
+  end
+end
